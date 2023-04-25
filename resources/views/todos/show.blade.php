d4b8e145f25b703396d17934d4e3c40c59c98732
manaspatil123 <manasbatam@gmail.com>
2023-04-25 19:26:02 +0530

    first commit

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <a href="{{ url()->previous() }}" class="btn btn-sm btn-info">Go Back</a><br>
+                    <b>Name of Todo: </b> {{$todo->title}}
+                    <br>
+                    <b>Todo description: </b> {{$todo->description}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
